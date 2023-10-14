5d40e360657977ed814c502eb443db5d4e90d7a3
Rafael Rodríguez <52231003+rr15rr@users.noreply.github.com>
2023-10-13 18:59:43 -0600

    Add files via upload
    This is the main file of the code for this program, any changes you might want to add feel free to make them, thanks.

---
+++ main.php
@@ -0,0 +1,45 @@
+<?php
+echo "Ingresa el numero que quieres convertido a Romano ";
+
+$numero = (int)readline();
+
+if($numero == 1) {
+  echo "I";
+}
+  
+if($numero == 2) {
+  echo "II";
+}
+
+if($numero == 3) {
+  echo "III";
+}
+
+if($numero == 4) {
+  echo "IV";
+}
+
+if($numero == 5) {
+  echo "V";
+}
+
+if($numero == 6) {
+  echo "VI";
+}
+
+if($numero == 7) {
+  echo "VII";
+}
+
+if($numero == 8) {
+  echo "VIII";
+}
+
+if($numero == 9){
+  echo "IX";
+}
+
+if($numero == 10){
+  echo "X";
+}
+?>
